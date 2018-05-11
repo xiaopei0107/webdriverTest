83f0638ded15ee90bceb505f275615ba87abeefb
xiaopei0107 <724152220@qq.com>
2018-05-11 15:19:09 +0800

    add...

---
+++ src/test/java/com/zxp/test/util/WebDriverUtil.java
@@ -0,0 +1,32 @@
+package com.zxp.test.util;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+public class WebDriverUtil {
+
+	private static WebDriver driver;
+
+	public static WebDriver getChromeWebDriver() {
+		String path = WebDriverUtil.class.getResource("/chromedriver.exe").getPath();
+		System.setProperty("webdriver.chrome.driver", path);
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(8, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		return driver;
+	}
+
+	public static WebDriver getIEWebDriver() {
+		String path = WebDriverUtil.class.getResource("/IEDriverServer.exe").getPath();
+		driver = new InternetExplorerDriver();
+		// 最大化窗口
+		driver.manage().window().maximize();
+		// 设置隐性等待时间
+		driver.manage().timeouts().implicitlyWait(8, TimeUnit.SECONDS);
+		return driver;
+	}
+
+}
