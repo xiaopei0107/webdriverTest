4fae8b8a533e122311f896a454174784eeb8cddb
xiaopei0107 <724152220@qq.com>
2018-05-23 10:50:34 +0800

    测试linux能跑脚本不

--- src/test/java/com/zxp/test/test/PassTest.java
+++ src/test/java/com/zxp/test/test/PassTest.java
@@ -1,7 +1,13 @@
 package com.zxp.test.test;
 
 import org.testng.annotations.Test;
+
+import com.zxp.test.util.WebDriverUtil;
+
 import org.testng.annotations.BeforeClass;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.Reporter;
 import org.testng.annotations.AfterClass;
@@ -16,7 +22,14 @@ public class PassTest {
   @Test
   public void isPassTest2() {
 	  Reporter.log("测试方法isPassTest2开始");
-	  Assert.assertEquals("1", "1");
+	  WebDriver driver = WebDriverUtil.getChromeWebDriver();
+	  driver.get("https://www.baidu.com/");
+	  
+	  WebElement kw = driver.findElement(By.id("kw"));
+	  Assert.assertEquals(kw.isDisplayed(), true);
+	  
+	  Reporter.log("验证通过");
+	  driver.quit();
 	  Reporter.log("测试方法isPassTest2结束");
   }
   @Test
--- src/test/java/com/zxp/test/util/WebDriverUtil.java
+++ src/test/java/com/zxp/test/util/WebDriverUtil.java
@@ -1,11 +1,12 @@
 package com.zxp.test.util;
 
 import java.util.ResourceBundle;
-import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.Reporter;
 
 public class WebDriverUtil {
@@ -14,10 +15,14 @@ public class WebDriverUtil {
 	private static String webDriverPath = ResourceBundle.getBundle("base").getString("webdriver.path");
 
 	public static WebDriver getChromeWebDriver() {
-		//String path = WebDriverUtil.class.getResource("/chromedriver.exe").getPath();
+		Reporter.log("初始化webdriver webDriverPath：" + webDriverPath);
+		// String path =
+		// WebDriverUtil.class.getResource("/chromedriver.exe").getPath();
 		System.setProperty("webdriver.chrome.driver", webDriverPath);
-		driver = new ChromeDriver();
-		driver.manage().timeouts().implicitlyWait(8, TimeUnit.SECONDS);
+
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("window-size=1280,728"); // 设置窗口大小
+		driver = new ChromeDriver(options);
 		driver.manage().window().maximize();
 		Reporter.log("初始化webdriver成功");
 		return driver;
@@ -29,7 +34,7 @@ public class WebDriverUtil {
 		// 最大化窗口
 		driver.manage().window().maximize();
 		// 设置隐性等待时间
-		driver.manage().timeouts().implicitlyWait(8, TimeUnit.SECONDS);
+		// driver.manage().timeouts().implicitlyWait(8, TimeUnit.SECONDS);
 		return driver;
 	}
 
